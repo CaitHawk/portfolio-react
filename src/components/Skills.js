0af72290097df439a72c1a1a2037ec4dfd1b4ed1
Caitlin Neathawk <neathawkcaitlin@gmail.com>
2022-06-19 14:56:53 -0700

    skills page desktop

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,36 @@
 import '../styles/Skills.css'
 
+const skillsArray = [
+    'HTML5',
+    'CSS3',
+    'Javascript',
+    'Sass/Scss',
+    'React',
+    'Next JS',
+    'Tailwind CSS',
+    'Bootstrap',
+    'Material UI',
+    'Gatsby',
+    'Node.js',
+    'Git',
+    'jQuery',
+    'Chakra UI'
+];
+
 function SkillsDesktop() {
     return (
         <section className="skillsDesktopMain" id="skillsDesktop">
-            <h1>Skills Desktop</h1>
+            <div className="skillsDesktopDesignDiv1"></div>
+            <div className="skillsDesktopDesignDiv2"></div>
+            <div className="skillsDesktopDiv">
+                <h2 className="skillsDesktopTitle">Tech Stack</h2>
+                <p className="skillsDesktopParagraph">Using the following languages & libraries, my passion has driven me to create beautiful, user friendly, responsive and accessible web applications</p>
+                <ul className="skillsDesktopUl">
+                    {skillsArray.map(skill => (
+                        <li className="skillsDesktopSkill" key={`${skillsArray.indexOf(skill)}skill`}>{skill}</li>
+                    ))}
+                </ul>
+            </div>
         </section>
     );
 }
