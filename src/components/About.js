aceea52ac97e51df6cef757aa34110f37b47b54c
Caitlin Neathawk <neathawkcaitlin@gmail.com>
2022-07-13 11:51:45 -0700

    resume link updated

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ function AboutSection() {
                     <p className="aboutInfoParagraph">Self-motivated self-starter. Nucamp Alumn. Myspace "about me" section pioneer. I've been coding as a hobby since 2019 and changed my career in 2020 after falling in love with all things web development. I currently work on the front-end as my asperations toward server side continues to grow. </p>
                     <h3 className="aboutInfoSubtitle">Personal Life</h3>
                     <p className="aboutInfoParagraph">Wife, mother, nature lover, activist, cat owner, island girl. Oahu native with a perfect work-life balance. I'm a puzzle master and logic guru who literally codes for fun. Nothing gets me more than a good book, a good laugh, and good food.</p>
-                    <a href="https://drive.google.com/file/d/1g0yeOSn1KuezP1f-jChWL-rpRXJUW9g3/view?usp=sharing"
+                    <a href="https://drive.google.com/file/d/1a-HbJkwbLGc2EhugDtR8SwrHKTUKx5PM/view?usp=sharing"
                         target="_blank" rel="noreferrer noopener" className="aboutInfoButton">VIEW MY RESUME</a>
                 </div>
                 <div className="aboutImgSocialDiv">
